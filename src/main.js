ee3f807ba998463cfc02ed5c523423acb68d427e
Vidiborets Viktor <vidiboretsv@gmail.com>
2024-08-18 11:38:11 +0300

    add the iziToast library for notifications

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@
 import { createGalleryCardTemplate } from './js/render-functions.js';
 import { fetchPhotos } from './js/pixabay-api.js';
 
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+
 
 const searchForm = document.querySelector('.search-form');
 console.log('searchForm:', searchForm);
@@ -11,23 +15,39 @@ console.log('gallery:', gallery);
 
 
 const onSearchFormSubmit = event => {
-  event.preventDefault();
+    event.preventDefault();
 
-  const searchedValue = searchForm.elements.user_query.value.trim();
+    const searchedValue = searchForm.elements.user_query.value.trim();
 
-  fetchPhotos(searchedValue)
-    .then(data => {
+    fetchPhotos(searchedValue)
+        .then(data => {
+                      
+            if (data.hits.length === 0) {
+                iziToast.error({
+                    message: 'Sorry, there are no images matchings your search query. Please try againe!',
+                    position: 'topRight',
+                });
 
-      const galleryCardsTemplate = data.hits.map(imgDetails => createGalleryCardTemplate(imgDetails)).join('');
+                gallery.innerHTML = '';
+                searchForm.reset();
 
-      gallery.innerHTML = galleryCardsTemplate;
+                return;
+            }
 
-    })
-    .catch(err => {
-      console.log(err);
-    })
+            const galleryCardsTemplate = data.hits.map(imgDetails => createGalleryCardTemplate(imgDetails)).join('');
 
-}
+            gallery.innerHTML = galleryCardsTemplate;
+
+        })
+        .catch(err => {
+            console.log(err);
+            iziToast.error({
+                message: 'Щось пішло не так. Спробуйте ще раз пізніше!',
+                position: 'topRight',
+            });
+        });
+
+};
 
 searchForm.addEventListener('submit', onSearchFormSubmit);
 
