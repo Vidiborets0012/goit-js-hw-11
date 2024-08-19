b82f98a23f4c13b377a20a37c584400e499b2210
Vidiborets Viktor <vidiboretsv@gmail.com>
2024-08-19 11:47:37 +0300

    add empty string check and change loader behavior

--- src/main.js
+++ src/main.js
@@ -23,11 +23,28 @@ const onSearchFormSubmit = event => {
     event.preventDefault();
 
     const searchedValue = searchForm.elements.user_query.value.trim();
+// Покращення:
+// якщо searchedValue є пробілом, функція fetchPhotos все одно викликається,
+// що призводить до непотрібного API запиту.
+// Потрібно searchedValue перед запитом на бекенд трімнути і перевірити на порожній рядок
+    
+    if (!searchedValue) {
+        iziToast.error({
+            message: 'Please enter a valid search query!',
+            position: 'topRight',
+            maxWidth: '500px',
+        });
+        return;
+    }
 
     loader.classList.remove('is-hidden');
 
+// коли з бекенда приходить порожній масив, не вимикається лоадер і постійно крутиться.
+
     fetchPhotos(searchedValue)
         .then(data => {
+            // вимкнути лоадер
+            loader.classList.add('is-hidden');
                       
             if (data.hits.length === 0) {
                 iziToast.error({
@@ -42,7 +59,7 @@ const onSearchFormSubmit = event => {
                 return;
             }
 
-            loader.classList.add('is-hidden');
+            // loader.classList.add('is-hidden');
 
             const galleryCardsTemplate = data.hits.map(imgDetails => createGalleryCardTemplate(imgDetails)).join('');
 
@@ -54,10 +71,11 @@ const onSearchFormSubmit = event => {
         .catch(err => {
             console.log(err);
             iziToast.error({
-                message: 'Щось пішло не так. Спробуйте ще раз пізніше!',
+                message: 'Something went wrong. Please try again later!',
                 position: 'topRight',
+                maxWidth: '500px',
             });
-
+            // вимкнути лоадер у випадку помилки
             loader.classList.add('is-hidden');
         });
 
